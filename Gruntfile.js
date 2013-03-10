d6cba3933e338b8a11d0c13bb014d845cf1efb0f
Liu Yang <zation1@gmail.com>
2013-03-10 13:49:12 +0800

    init project

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+  var dependencies = [
+    'components/jquery/jquery.js',
+    'components/underscore/underscore.js',
+    'components/backbone/backbone.js'],
+
+    sources = 'app/js/**/*.js',
+
+    specs = 'spec/*Spec.js';
+
+  grunt.initConfig({
+    concat: {
+      development: {
+        src: dependencies,
+        dest: 'app/js/lib.js'
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'app/',
+          keepalive: true
+        }
+      },
+      spec: {
+        options: {
+          port: 9001,
+          base: './',
+          keepalive: true
+        }
+      }
+    },
+
+    jasmine: {
+      test: {
+        src: sources,
+        options: {
+          specs: specs,
+          vendor: 'app/js/lib.js'
+        }
+      }
+    },
+
+    compass: {
+      development: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'app/css'
+        }
+      }
+    },
+
+    watch: {
+      test: {
+        files: [sources, specs],
+        tasks: ['jasmine:test:build']
+      },
+      css: {
+        files: 'sass/**/*',
+        tasks: 'compass:development'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+};
---
+++ spec/mainSpec.js
@@ -0,0 +1,9 @@
+describe('test', function() {
+  it('should success', function() {
+    expect(1).toBe(1);
+  });
+
+  it('should failed', function() {
+    expect(2).toBe(3);
+  });
+});
