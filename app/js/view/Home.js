2b61a7c3bd8c1fc2514de66d1fcf1f60032e60c6
Liu Yang <zation1@gmail.com>
2013-03-20 13:58:11 +0800

    add draft logo in home

--- app/js/view/Home.js
+++ app/js/view/Home.js
@@ -1,6 +1,8 @@
 app.view.Home = Backbone.View.extend({
   className: 'home',
   initialize: function() {
+    this.template = _.template($('#home').html());
+
     var addTaskView = new app.view.AddTask({
       collection: this.collection
     });
@@ -8,6 +10,7 @@ app.view.Home = Backbone.View.extend({
     var taskListView = new app.view.TaskList({
       collection: this.collection
     });
+    this.$el.append(this.template());
     this.$el.append(taskListView.$el);
     this.$el.append(addTaskView.$el);
   }
