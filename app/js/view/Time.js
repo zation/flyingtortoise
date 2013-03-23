6f597bb1d8c3ae682bf6a6dbd59ae9102d78bf27
Liu Yang <zation1@gmail.com>
2013-03-23 15:34:12 +0800

    display time in time view

--- app/js/view/Time.js
+++ app/js/view/Time.js
@@ -1,25 +1,53 @@
-app.view.Time = Backbone.View.extend({
-  className: 'time',
-  events: {
-    'click .stop': 'stopTask'
-  },
-  stopTask: function() {
-    this.model.stop(moment());
-    this.model.save();
-    this.$el.removeClass('task-order-' + this.model.get('order'));
-    this.$el.hide();
-    app.Event.trigger(app.Event.TaskStop);
-  },
-  startTask: function(model) {
-    this.$el.html(this.template(model.attributes));
-    this.$el.addClass('task-order-' + model.get('order'));
-    this.$el.show();
-    this.model = model;
-    this.model.start(moment());
-  },
-  initialize: function() {
-    this.template = _.template($('#time').html());
-    this.$el.hide();
-    app.Event.on(app.Event.TaskStart, this.startTask, this);
-  }
-});
+(function() {
+  var recorderTimer;
+
+  app.view.Time = Backbone.View.extend({
+    className: 'time',
+    events: {
+      'click .stop': 'stopTask'
+    },
+    stopTask: function() {
+      this.model.stop(moment());
+      this.model.save();
+      this.$el.removeClass('task-order-' + this.model.get('order'));
+      this.$el.hide();
+      app.Event.trigger(app.Event.TaskStop);
+    },
+    startTask: function(model) {
+      this.$el.html(this.template(model.attributes));
+      this.$el.addClass('task-order-' + model.get('order'));
+      this.$el.show();
+      var startTime = moment();
+      var $timeElement = this.$el.find('time');
+      var second = 0;
+      var minute = 0;
+      var secondDisplay;
+      var minuteDisplay;
+      recorderTimer = setInterval(displayTime, 1000);
+      this.model = model;
+      this.model.start(startTime);
+
+      function displayTime() {
+        second++;
+        if (second > 59) {
+          minute++;
+          second = 0;
+        }
+        secondDisplay = second.toString();
+        minuteDisplay = minute.toString();
+        if (second < 10) {
+          secondDisplay = '0' + secondDisplay;
+        }
+        if (minute < 10) {
+          minuteDisplay = '0' + minuteDisplay;
+        }
+        $timeElement.text(minuteDisplay + ':' + secondDisplay);
+      }
+    },
+    initialize: function() {
+      this.template = _.template($('#time').html());
+      this.$el.hide();
+      app.Event.on(app.Event.TaskStart, this.startTask, this);
+    }
+  });
+})();
--- spec/view/TimeSpec.js
+++ spec/view/TimeSpec.js
@@ -63,4 +63,35 @@ describe('Time View', function() {
     expect(timeView.$el).toBeHidden();
     expect(app.Event.trigger).toHaveBeenCalledWith(app.Event.TaskStop);
   });
+
+  describe('should display time', function() {
+    beforeEach(function() {
+      jasmine.Clock.useMock();
+      timeView.startTask(taskModel);
+    });
+
+    it('when seconds are less than 10', function() {
+      jasmine.Clock.tick(7000);
+
+      expect(timeView.$el.find('time')).toHaveText('00:07');
+    });
+
+    it('when seconds are more than 10', function() {
+      jasmine.Clock.tick(17000);
+
+      expect(timeView.$el.find('time')).toHaveText('00:17');
+    });
+
+    it('when minutes are less than 10', function() {
+      jasmine.Clock.tick(70000);
+
+      expect(timeView.$el.find('time')).toHaveText('01:10');
+    });
+
+    it('when minutes are more than 10', function() {
+      jasmine.Clock.tick(700000);
+
+      expect(timeView.$el.find('time')).toHaveText('11:40');
+    });
+  });
 });
