c123517c13d256b6f35a8f2debd5eb3c1f6047f5
Liu Yang <zation1@gmail.com>
2013-03-24 22:18:39 +0800

    add state view

--- app/js/view/State.js
+++ app/js/view/State.js
@@ -1,15 +1,17 @@
-app.view.State = Backbone.View.extend({
-  className: 'state',
-  onRotate: function(orientation) {
-    if (orientation === 0) {
+(function() {
+  app.view.State = Backbone.View.extend({
+    className: 'state',
+    onRotate: function(orientation) {
+      if (orientation === 0) {
+        this.$el.hide();
+      }
+      else if (Math.abs(orientation) === 90) {
+        this.$el.show();
+      }
+    },
+    initialize: function() {
       this.$el.hide();
+      app.Event.on(app.Event.Rotate, this.onRotate, this);
     }
-    else if (Math.abs(orientation) === 90) {
-      this.$el.show();
-    }
-  },
-  initialize: function() {
-    this.$el.hide();
-    app.Event.on(app.Event.Rotate, this.onRotate, this);
-  }
-});
+  });
+})();
