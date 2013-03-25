6d8be2df7d2351fc697afb1990987eee7baa5638
Liu Yang <zation1@gmail.com>
2013-03-25 14:59:49 +0800

    change state task name when switch

--- app/js/view/StateTaskName.js
+++ app/js/view/StateTaskName.js
@@ -1,7 +1,11 @@
 app.view.StateTaskName = Backbone.View.extend({
   className: 'task-name',
   tagName: 'span',
+  renderTaskName: function(model) {
+    this.$el.text(model.get('name'));
+  },
   initialize: function() {
-    this.$el.text(this.model.get('name'));
+    this.renderTaskName(this.model);
+    app.Event.on(app.Event.Switch, this.renderTaskName, this);
   }
 });
--- spec/view/StateTaskNameSpec.js
+++ spec/view/StateTaskNameSpec.js
@@ -1,4 +1,8 @@
 describe('StateTaskName View', function() {
+  beforeEach(function() {
+
+  });
+
   it('should render the task name', function() {
     var expectedTaskName = 'name';
     var stateTaskNameView = new app.view.StateTaskName({
@@ -6,6 +10,18 @@ describe('StateTaskName View', function() {
         name: expectedTaskName
       })
     });
+
+    expect(stateTaskNameView.$el).toHaveText(expectedTaskName);
+  });
+
+  it('should change task name when switch to other task', function() {
+    var stateTaskNameView = new app.view.StateTaskName({
+      model: new app.model.Task()
+    });
+    var expectedTaskName = 'other name';
+    app.Event.trigger(app.Event.Switch, new app.model.Task({
+      name: expectedTaskName
+    }));
 
     expect(stateTaskNameView.$el).toHaveText(expectedTaskName);
   });
