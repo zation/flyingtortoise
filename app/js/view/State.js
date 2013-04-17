929362fc72a3706894169438d019566e9207b21d
Liu Yang <zation1@gmail.com>
2013-04-17 16:00:25 +0800

    prevent duplicate state view adding in iOS mobile device

--- app/js/view/State.js
+++ app/js/view/State.js
@@ -9,20 +9,22 @@ app.view.State = Backbone.View.extend({
     }
   },
   render: function() {
-    this.$el.show();
-    var switchers = new app.view.Switchers({
-      collection: this.collection
-    });
-    this.$el.append(switchers.$el);
-    var firstTask = this.collection.at(0);
-    var stateTaskNameView = new app.view.StateTaskName({
-      model: firstTask
-    });
-    this.$el.append(stateTaskNameView.$el);
-    var chartView = new app.view.Chart({
-      model: firstTask
-    });
-    this.$el.append(chartView.$el);
+    if (this.$el.is(':hidden')) {
+      this.$el.show();
+      var switchers = new app.view.Switchers({
+        collection: this.collection
+      });
+      this.$el.append(switchers.$el);
+      var firstTask = this.collection.at(0);
+      var stateTaskNameView = new app.view.StateTaskName({
+        model: firstTask
+      });
+      this.$el.append(stateTaskNameView.$el);
+      var chartView = new app.view.Chart({
+        model: firstTask
+      });
+      this.$el.append(chartView.$el);
+    }
   },
   initialize: function() {
     this.$el.hide();
